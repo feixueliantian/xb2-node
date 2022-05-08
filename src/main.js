a21b3a0d8e1d944d67420d481f6c2ea32a4a455e
于曼丽 <yumanli@126.com>
2022-05-08 09:28:33 +0800

    创建 Web 服务器 <Express.js>

--- src/main.js
+++ src/main.js
@@ -1,24 +1,7 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((request, response) => {
-    switch (request.url) {
-        case '/':
-            response.write('hello ~');
-            break;
-        case '/posts':
-            response.write('posts');
-            break;
-        case '/signup':
-            response.write('signup');
-            break;
-        default:
-            response.writeHead(404);
-            response.write('404');
-            break;
-    }
-    response.end();
-});
-
-server.listen(3000, () => {
-    console.log('服务已启动');
+app.listen(port, () => {
+    console.log('服务已启动!');
 });
