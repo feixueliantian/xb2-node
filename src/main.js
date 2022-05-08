a9b21e1755ff0c6bf6afb610179915d6050b1e0c
于曼丽 <yumanli@126.com>
2022-05-08 09:36:38 +0800

    准备演示数据

--- src/main.js
+++ src/main.js
@@ -3,9 +3,27 @@ const app = express();
 const port = 3000;
 
 app.listen(port, () => {
-    console.log('服务已启动!');
+  console.log('服务已启动!');
 });
 
 app.get('/', (request, response) => {
-    response.send('你好');
+  response.send('你好');
 });
+
+const data = [
+  {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山，苍茫云海间',
+  },
+  {
+    id: 2,
+    title: '望岳',
+    content: '会当凌绝顶，一览众山小',
+  },
+  {
+    id: 3,
+    title: '忆江南',
+    content: '日出江花红胜火，春来江水绿如蓝',
+  },
+];
