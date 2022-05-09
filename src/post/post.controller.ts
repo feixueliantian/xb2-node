73fa72915fedc6102b4fee688cea5a4989fb3028
于曼丽 <yumanli@126.com>
2022-05-09 11:59:43 +0800

    测试使用异常处理器

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,7 +1,14 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { getPosts } from './post.service';
 
-export const index = (request: Request, response: Response): void => {
+export const index = (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void => {
+  if (request.get('authorization') !== 'SECRET') {
+    return next(new Error());
+  }
   const posts = getPosts();
   response.send(posts);
 };
