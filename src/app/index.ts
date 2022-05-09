410721b7c9b2215bce39247d83bd4fe26c68730b
于曼丽 <yumanli@126.com>
2022-05-09 11:47:23 +0800

    创建默认的异常处理器

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,13 @@
 import express = require('express');
 import postRouter from '../post/post.router';
+import { defaultErrorHandler } from './app.middleware';
 
 const app = express();
 
 app.use(express.json());
+
 app.use(postRouter);
+
+app.use(defaultErrorHandler);
 
 export default app;
