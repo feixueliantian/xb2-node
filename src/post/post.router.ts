441b2d54fb7f9a199111f5b2758e7ee335b7a329
于曼丽 <yumanli@126.com>
2022-05-09 21:53:11 +0800

    测试更新内容接口

--- src/post/post.router.ts
+++ src/post/post.router.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get('/posts', requestUrl, postController.index);
 router.post('/posts', postController.store);
+router.patch('/posts/:postId', postController.update);
 
 export default router;
