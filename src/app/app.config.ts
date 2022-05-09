d43ca8258b79f573ea9d3ed5059b7923a97b200e
于曼丽 <yumanli@126.com>
2022-05-09 17:51:02 +0800

    安装驱动连接数据仓库

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,3 +3,11 @@ import { config } from 'dotenv';
 config();
 
 export const { APP_PORT } = process.env;
+
+export const {
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
+} = process.env;
