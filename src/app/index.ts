69daf91190e6967a03e1e375eafa7e5e96ea47ae
于曼丽 <yumanli@126.com>
2022-05-09 11:33:19 +0800

    路由：Router

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,9 @@
 import express = require('express');
+import postRouter from '../post/post.router';
 
 const app = express();
 
 app.use(express.json());
+app.use(postRouter);
 
 export default app;
---
+++ src/post/post.router.ts
@@ -0,0 +1,7 @@
+import express = require('express');
+import * as postController from './post.controller';
+
+const router = express.Router();
+router.get('/posts', postController.index);
+
+export default router;
