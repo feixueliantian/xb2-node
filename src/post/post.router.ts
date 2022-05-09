a8726308179acfe08c59e491935f22a5be0b7c37
于曼丽 <yumanli@126.com>
2022-05-09 22:05:04 +0800

    删除内容

--- src/post/post.router.ts
+++ src/post/post.router.ts
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/posts', requestUrl, postController.index);
 router.post('/posts', postController.store);
 router.patch('/posts/:postId', postController.update);
+router.delete('/posts/:postId', postController.destroy);
 
 export default router;
