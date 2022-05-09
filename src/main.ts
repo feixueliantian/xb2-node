e60975e1bbdf5e116598e80a29cbea7a77e435c0
于曼丽 <yumanli@126.com>
2022-05-09 09:53:36 +0800

    自动编译与重启

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.json());
 
 app.listen(port, () => {
-  console.log('服务已启动!');
+  console.log('服务已启动 ~');
 });
 
 app.get('/', (request: Request, response: Response) => {
