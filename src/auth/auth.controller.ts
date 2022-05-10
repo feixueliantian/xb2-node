1efbd5a18975abca606f33963abbcf854c33f557
于曼丽 <yumanli@126.com>
2022-05-10 11:37:23 +0800

    签发 JWT 令牌

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
+import { signToken } from './auth.service';
 
 export const login = async (
   request: Request,
   response: Response,
   next: NextFunction,
 ) => {
-  const { name, password } = request.body;
-  response.send({ message: `欢迎回来，${name}` });
+  const {
+    user: { id, name },
+  } = request.body;
+
+  const payload = { id, name };
+
+  try {
+    const token = signToken({ payload });
+    response.send({ id, name, token });
+  } catch (error) {
+    next(error);
+  }
 };
