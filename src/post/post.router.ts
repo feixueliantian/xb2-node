86f65cdc98fbae397c446e80a8388e5988a8584b
于曼丽 <yumanli@126.com>
2022-05-10 15:27:41 +0800

    创建内容时记录内容的作者

--- src/post/post.router.ts
+++ src/post/post.router.ts
@@ -1,11 +1,12 @@
 import express = require('express');
 import * as postController from './post.controller';
 import { requestUrl } from '../app/app.middleware';
+import { authGuard } from '../auth/auth.middleware';
 
 const router = express.Router();
 
 router.get('/posts', requestUrl, postController.index);
-router.post('/posts', postController.store);
+router.post('/posts', authGuard, postController.store);
 router.patch('/posts/:postId', postController.update);
 router.delete('/posts/:postId', postController.destroy);
 
