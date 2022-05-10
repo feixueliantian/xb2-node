9c17bf39e583083bdfbcd9cbe3415e7575e19337
于曼丽 <yumanli@126.com>
2022-05-10 09:15:45 +0800

    定义创建用户接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,3 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 import { UserModel } from './user.model';
 import * as userServer from './user.service';
+
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  const { name, password } = request.body;
+
+  try {
+    const data = await userServer.createUser({ name, password });
+    response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -3,4 +3,6 @@ import * as userController from './user.controller';
 
 const router = express.Router();
 
+router.post('/users', userController.store);
+
 export default router;
