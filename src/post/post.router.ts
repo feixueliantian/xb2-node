bc2853486e5843aeb8241b920da328cabd06e765
于曼丽 <yumanli@126.com>
2022-05-13 15:26:00 +0800

    配置接口的访问控制

--- src/post/post.router.ts
+++ src/post/post.router.ts
@@ -1,13 +1,23 @@
 import express = require('express');
 import * as postController from './post.controller';
 import { requestUrl } from '../app/app.middleware';
-import { authGuard } from '../auth/auth.middleware';
+import { authGuard, accessControl } from '../auth/auth.middleware';
 
 const router = express.Router();
 
 router.get('/posts', requestUrl, postController.index);
 router.post('/posts', authGuard, postController.store);
-router.patch('/posts/:postId', postController.update);
-router.delete('/posts/:postId', postController.destroy);
+router.patch(
+  '/posts/:postId',
+  authGuard,
+  accessControl({ possession: true }),
+  postController.update,
+);
+router.delete(
+  '/posts/:postId',
+  authGuard,
+  accessControl({ possession: true }),
+  postController.destroy,
+);
 
 export default router;
