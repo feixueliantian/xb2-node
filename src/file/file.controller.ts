890aeb34b5b72feca5f5c3bd75dc50c65afe41c6
于曼丽 <yumanli@126.com>
2022-05-13 17:29:58 +0800

    存储上传文件信息

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import _ = require('lodash');
+import { createFile } from './file.service';
 
 /**
  * 上传文件
@@ -8,6 +10,29 @@ export const store = async (
   response: Response,
   next: NextFunction,
 ) => {
-  console.log(request.file);
-  response.sendStatus(200);
+  // 当前用户
+  const { id: userId } = request.user;
+
+  // 所属内容
+  const postId = +request.query.post;
+
+  // 文件信息
+  const fileInfo = _.pick(request.file, [
+    'originalname',
+    'mimetype',
+    'filename',
+    'size',
+  ]);
+
+  try {
+    const data = await createFile({
+      ...fileInfo,
+      userId,
+      postId,
+    });
+
+    response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
 };
