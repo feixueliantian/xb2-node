d8a04c846f56912cc5e81996e3f6bacc7132bf36
于曼丽 <yumanli@126.com>
2022-05-13 17:02:38 +0800

    定义上传文件接口

--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -1,6 +1,10 @@
 import express = require('express');
 import * as fileController from './file.controller';
+import { authGuard } from '../auth/auth.middleware';
+import { fileInterceptor } from './file.middleware';
 
 const router = express.Router();
+
+router.post('/files', authGuard, fileInterceptor, fileController.store);
 
 export default router;
