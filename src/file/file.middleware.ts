de25bee4f9d86c3ebbca9000e4e822af9595b7c2
于曼丽 <yumanli@126.com>
2022-05-13 19:17:12 +0800

    图像信息：读取图像文件附加信息

--- src/file/file.middleware.ts
+++ src/file/file.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import multer = require('multer');
+import Jimp = require('jimp');
 
 /**
  * 创建一个 Multer
@@ -12,3 +13,25 @@ const fileUpload = multer({
  * 文件拦截器
  */
 export const fileInterceptor = fileUpload.single('file');
+
+/**
+ * 文件处理器
+ */
+export const fileProcessor = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  const { path } = request.file;
+
+  let image: Jimp;
+
+  try {
+    image = await Jimp.read(path);
+  } catch (error) {
+    return next(error);
+  }
+
+  console.log(image);
+  next();
+};
--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -1,11 +1,17 @@
 import express = require('express');
 import * as fileController from './file.controller';
 import { authGuard } from '../auth/auth.middleware';
-import { fileInterceptor } from './file.middleware';
+import { fileInterceptor, fileProcessor } from './file.middleware';
 
 const router = express.Router();
 
-router.post('/files', authGuard, fileInterceptor, fileController.store);
+router.post(
+  '/files',
+  authGuard,
+  fileInterceptor,
+  fileProcessor,
+  fileController.store,
+);
 router.get('/files/:fileId/serve', fileController.serve);
 
 export default router;
