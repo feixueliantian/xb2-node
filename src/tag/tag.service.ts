8bd2f718810e0fba30b17ad3a8fbf0083f837ee4
于曼丽 <yumanli@126.com>
2022-05-19 16:44:27 +0800

    定义检查内容标签服务

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -37,6 +37,19 @@ export const createPostTag = async (postId: number, tagId: number) => {
     VALUES (?, ?)
   `;
 
-  const [data] = await connection.promise().query(statement);
+  const [data] = await connection.promise().query(statement, [postId, tagId]);
   return data;
+};
+
+/**
+ * 检查内容标签
+ */
+export const postHasTag = async (postId: number, tagId: number) => {
+  const statement = `
+    SELECT * FROM post_tag
+    WHERE postId = ? AND tagId = ?
+  `;
+
+  const [data] = await connection.promise().query(statement, [postId, tagId]);
+  return data[0] ? true : false;
 };
