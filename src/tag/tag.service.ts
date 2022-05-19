a38fcea57cf54feecfca4c7d8b6a77f3e7b7a1ef
于曼丽 <yumanli@126.com>
2022-05-19 15:53:36 +0800

    定义存储标签数据的功能

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,1 +1,12 @@
 import { connection } from '../app/database/mysql';
+import { TagModel } from './tag.model';
+
+export const createTag = async (tag: TagModel) => {
+  const statement = `
+    INSERT INTO tag
+    SET ? 
+  `;
+
+  const [data] = await connection.promise().query(statement, tag);
+  return data as any;
+};
