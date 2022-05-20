d4deda13e20e180b54afe57eb6190d76b97f15fc
于曼丽 <yumanli@126.com>
2022-05-20 08:33:47 +0800

    定义发表评论用的接口

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,1 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
+import { createComment } from './comment.service';
+
+/**
+ * 发表评论
+ */
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  const { id: userId } = request.user;
+  const { content, postId } = request.body;
+  const comment = {
+    content,
+    postId,
+    userId,
+  };
+
+  try {
+    const data = await createComment(comment);
+    return response.status(201).send(data);
+  } catch (error) {
+    return next(error);
+  }
+};
