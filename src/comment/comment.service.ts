10c2049fbfa7928afc3e5d10881f204e768f4087
于曼丽 <yumanli@126.com>
2022-05-20 08:46:28 +0800

    定义检查评论是否为回复评论的功能

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -13,3 +13,16 @@ export const createComment = async (comment: CommentModel) => {
   const [data] = await connection.promise().query(statement, comment);
   return data;
 };
+
+/**
+ * 检查评论是否为回复评论
+ */
+export const isReplyComment = async (commentId: number) => {
+  const statement = `
+    SELECT parentId FROM comment
+    WHERE id = ?
+  `;
+
+  const [data] = await connection.promise().query(statement);
+  return data[0].parentId ? true : false;
+};
