5dda6ade4647b1da58cb3a53c236f30619b3a022
于曼丽 <yumanli@126.com>
2022-05-22 11:25:32 +0800

    在内容列表中包含内容的相关文件

--- src/post/post.provider.ts
+++ src/post/post.provider.ts
@@ -16,4 +16,27 @@ export const sqlFragment = {
       WHERE comment.postId = post.id
     ) as totalComments
   `,
+  leftJoinOneFile: `
+      LEFT JOIN LATERAL (
+        SELECT * FROM file
+        WHERE file.postId = post.id
+        ORDER BY file.id DESC
+        LIMIT 1
+      ) AS file ON file.postId = post.id
+  `,
+  file: `
+    CAST(
+      IF(
+        COUNT(file.id),
+        GROUP_CONCAT(
+          DISTINCT JSON_OBJECT(
+            'id', file.id,
+            'width', file.width,
+            'height', file.height
+          )
+        ),
+        NULL
+      ) AS JSON
+    ) as file
+  `,
 };
