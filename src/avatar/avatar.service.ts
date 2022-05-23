3c9b9362d500d4855b8c694f82013aca62636f52
于曼丽 <yumanli@126.com>
2022-05-23 10:20:28 +0800

    定义按用户 ID 查找头像的功能

--- src/avatar/avatar.service.ts
+++ src/avatar/avatar.service.ts
@@ -13,3 +13,18 @@ export const createAvatar = async (avatar: AvatarModel) => {
   const [data] = await connection.promise().query(statement, avatar);
   return data;
 };
+
+/**
+ * 按照用户 ID 查找头像
+ */
+export const getAvatarByUserId = async (userId: number) => {
+  const statement = `
+    SELECT * FROM avatar
+    WHERE userId = ?
+    ORDER BY id DESC
+    limit 1
+  `;
+
+  const [data] = await connection.promise().query(statement, userId);
+  return data;
+};
