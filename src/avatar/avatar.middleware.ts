bf244906026f2062c85be7883713d41921fae6c5
于曼丽 <yumanli@126.com>
2022-05-23 10:01:55 +0800

    调整头像的尺寸

--- src/avatar/avatar.middleware.ts
+++ src/avatar/avatar.middleware.ts
@@ -1,6 +1,8 @@
+import path = require('path');
 import { Request, Response, NextFunction } from 'express';
 import multer = require('multer');
 import { fileFilter } from '../file/file.middleware';
+import Jimp = require('jimp');
 
 const avatarUploadFilter = fileFilter(['image/png', 'image/jpg', 'image/jpeg']);
 const avatarUpload = multer({
@@ -9,3 +11,25 @@ const avatarUpload = multer({
 });
 
 export const avatarInterceptor = avatarUpload.single('avatar');
+
+/**
+ * 处理头像
+ */
+export const avatarProcessor = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  const { file } = request;
+  const filePath = path.join(file.destination, 'resized', file.filename);
+
+  try {
+    const image = await Jimp.read(file.path);
+    image.resize(256, 256).quality(85).write(`${filePath}-large`);
+    image.resize(128, 128).quality(85).write(`${filePath}-medium`);
+    image.resize(64, 64).quality(85).write(`${filePath}-thumbnail`);
+  } catch (error) {
+    return next(error);
+  }
+  return next();
+};
--- src/avatar/avatar.router.ts
+++ src/avatar/avatar.router.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { authGuard } from '../auth/auth.middleware';
 import * as avatarController from './avatar.controller';
-import { avatarInterceptor } from './avatar.middleware';
+import { avatarInterceptor, avatarProcessor } from './avatar.middleware';
 
 const router = Router();
 
-router.post('/avatar', authGuard, avatarInterceptor, avatarController.store);
+router.post(
+  '/avatar',
+  authGuard,
+  avatarInterceptor,
+  avatarProcessor,
+  avatarController.store,
+);
 
 export default router;
