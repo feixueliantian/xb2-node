b32c7fffb21fccfe65fd1699c581549dad601c83
于曼丽 <yumanli@126.com>
2022-05-24 17:21:02 +0800

    获取用户的评论列表

--- src/comment/comment.middleware.ts
+++ src/comment/comment.middleware.ts
@@ -10,16 +10,27 @@ export const filter = async (
 ) => {
   const { post, user, action } = request.query;
 
+  // 默认查看所有评论，不包括回复
   request.filter = {
     name: 'default',
     sql: 'comment.parentId IS NULL',
   };
 
+  // 查看内容的评论
   if (post && !user && !action) {
     request.filter = {
       name: 'postComments',
       sql: 'comment.parentId IS NULL AND comment.postId = ?',
       params: post as string,
+    };
+  }
+
+  // 查看用户的评论
+  if (user && action === 'published' && !post) {
+    request.filter = {
+      name: 'userComments',
+      sql: 'comment.parentId IS NULL AND comment.userId = ?',
+      params: user as string,
     };
   }
 
