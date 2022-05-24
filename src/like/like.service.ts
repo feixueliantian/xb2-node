61e0182b2575c2cfdcd8e67c4c5b41820a876bee
于曼丽 <yumanli@126.com>
2022-05-24 09:09:29 +0800

    定义取消点赞内容功能

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -13,3 +13,16 @@ export const createUserLikePost = async (userId: number, postId: number) => {
   const [data] = await connection.promise().query(statement, [userId, postId]);
   return data;
 };
+
+/**
+ * 取消用户点赞内容
+ */
+export const deleteUserLikePost = async (userId: number, postId: number) => {
+  const statement = `
+    DELETE FROM user_like_post
+    WHERE userId = ? AND postId = ?
+  `;
+
+  const [data] = await connection.promise().query(statement, [userId, postId]);
+  return data;
+};
