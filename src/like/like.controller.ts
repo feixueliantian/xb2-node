40f67022c20aee531908de72d4866184c1940552
于曼丽 <yumanli@126.com>
2022-05-24 09:04:54 +0800

    定义点赞内容接口

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,1 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
+import { createUserLikePost } from './like.service';
+
+/**
+ * 点赞内容
+ */
+export const storeUserLikePost = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  const { postId } = request.params;
+  const { id: userId } = request.user;
+
+  try {
+    const data = await createUserLikePost(userId, parseInt(postId, 10));
+    response.status(201).send(data);
+  } catch (error) {
+    return next(error);
+  }
+};
--- src/like/like.router.ts
+++ src/like/like.router.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { authGuard } from '../auth/auth.middleware';
 import * as likeController from './like.controller';
 
 const router = Router();
+
+router.post('/posts/:postId/like', authGuard, likeController.storeUserLikePost);
 export default router;
