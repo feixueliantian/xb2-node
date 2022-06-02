45fa3622354cc93ee6341f63269713448854c56f
于曼丽 <yumanli@126.com>
2022-06-02 14:36:12 +0800

    创建 Socket.io 实时服务器

--- src/app/app.server.ts
+++ src/app/app.server.ts
@@ -1,5 +1,21 @@
 import http = require('http');
+import { Server } from 'socket.io';
+import { ALLOW_ORIGIN } from './app.config';
 import app from './index';
 
+/**
+ * HTTP 服务器
+ */
 const httpServer = http.createServer(app);
+
+/**
+ * IO 服务器
+ */
+export const socketIoServer = new Server(httpServer, {
+  cors: {
+    origin: ALLOW_ORIGIN,
+    allowedHeaders: ['X-TOTAL-COUNT'],
+  },
+});
+
 export default httpServer;
