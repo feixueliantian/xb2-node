36595758990052807ff3ce3ba4bf7d07f601f6dd
于曼丽 <yumanli@126.com>
2022-06-02 17:13:56 +0800

    定义搜索标签接口

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,1 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
+import { searchTags } from './search.service';
+
+/**
+ * 搜索标签
+ */
+export const tags = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  const name = request.query.name as string;
+
+  try {
+    const tags = await searchTags({ name });
+    return response.send(tags);
+  } catch (error) {
+    return next(error);
+  }
+};
--- src/search/search.router.ts
+++ src/search/search.router.ts
@@ -2,4 +2,7 @@ import { Router } from 'express';
 import * as searchController from './search.controller';
 
 const router = Router();
+
+router.get('/search/tags', searchController.tags);
+
 export default router;
