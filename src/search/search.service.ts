c3b96c551d24ba61f130a4a1f1f4838938ae8598
于曼丽 <yumanli@126.com>
2022-06-02 17:22:14 +0800

    定义搜索用户服务

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,12 +1,12 @@
 import { connection } from '../app/database/mysql';
 
+/**
+ * 搜索标签
+ */
 interface SearchTagsOptions {
   name?: string;
 }
 
-/**
- * 按照标签名称搜索标签
- */
 export const searchTags = async (options: SearchTagsOptions) => {
   const { name } = options;
 
@@ -33,4 +33,42 @@ export const searchTags = async (options: SearchTagsOptions) => {
 
   const [data] = await connection.promise().query(statement, params);
   return data as any;
+};
+
+/**
+ * 搜索用户
+ */
+interface SearchUsersOptions {
+  name?: string;
+}
+
+export const searchUsers = async (options: SearchUsersOptions) => {
+  const { name } = options;
+
+  const params = [`%${name}%`];
+
+  const statement = `
+    SELECT
+      user.id,
+      user.name,
+      IF(COUNT(avatar.id) ? 1 : NULL) as avatar
+      (
+        SELECT COUNT(post.id)
+        FROM post
+        WHERE post.userId = user.id
+      ) as totalPosts
+    FROM
+      user
+    LEFT JOIN
+      avatar ON avatar.userId = user.id
+    WHERE
+      user.name like ?
+    GROUP BY
+      user.id
+    LIMIT
+      10
+  `;
+
+  const [data] = await connection.promise().query(statement, params);
+  return data;
 };
