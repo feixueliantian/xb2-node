2e740a61464766dcdd2e9a819cfc99d00f633dcf
于曼丽 <yumanli@126.com>
2022-06-04 17:36:34 +0800

    记录评论相关接口的使用情况

--- src/comment/comment.router.ts
+++ src/comment/comment.router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { accessLog } from '../access-log/access-log.middleware';
 import { COMMENTS_PER_PAGE } from '../app/app.config';
 import { accessControl, authGuard } from '../auth/auth.middleware';
 import { paginate } from '../post/post.middleware';
@@ -7,26 +8,79 @@ import { filter } from './comment.middleware';
 
 const router = Router();
 
-router.post('/comments', authGuard, commentController.store);
-router.post('/comments/:commentId/reply', authGuard, commentController.reply);
+// 发表评论
+router.post(
+  '/comments',
+  authGuard,
+  accessLog({
+    action: 'createComment',
+    resourceType: 'comment',
+    payloadParam: 'body.content',
+  }),
+  commentController.store,
+);
+
+// 回复评论
+router.post(
+  '/comments/:commentId/reply',
+  authGuard,
+  accessLog({
+    action: 'createCommentReply',
+    resourceType: 'comment',
+    resourceParamName: 'commentId',
+    payloadParam: 'body.content',
+  }),
+  commentController.reply,
+);
+
+// 修改评论
 router.patch(
   '/comments/:commentId',
   authGuard,
   accessControl({ possession: true }),
+  accessLog({
+    action: 'updateComment',
+    resourceType: 'comment',
+    resourceParamName: 'commentId',
+    payloadParam: 'body.content',
+  }),
   commentController.update,
 );
+
+// 删除评论
 router.delete(
   '/comments/:commentId',
   authGuard,
   accessControl({ possession: true }),
+  accessLog({
+    action: 'deleteComment',
+    resourceType: 'comment',
+    resourceParamName: 'commentId',
+  }),
   commentController.destroy,
 );
+
+// 评论列表
 router.get(
   '/comments',
   filter,
   paginate(COMMENTS_PER_PAGE),
+  accessLog({
+    action: 'getComments',
+    resourceType: 'comment',
+  }),
   commentController.index,
 );
-router.get('/comments/:commentId/replies', commentController.indexReplies);
+
+// 回复列表
+router.get(
+  '/comments/:commentId/replies',
+  accessLog({
+    action: 'getCommentReplies',
+    resourceType: 'comment',
+    resourceParamName: 'commentId',
+  }),
+  commentController.indexReplies,
+);
 
 export default router;
