0476704fb9d2b5c09dbbea48c185f410913af07a
于曼丽 <yumanli@126.com>
2022-06-04 10:33:53 +0800

    定义创建审核日志接口

--- src/audit-log/audit-log.router.ts
+++ src/audit-log/audit-log.router.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
+import { authGuard } from '../auth/auth.middleware';
 import * as auditLogController from './audit-log.controller';
+import { auditLogGuard } from './audit-log.middleware';
 
 const router = Router();
+
+router.post('/audit-logs', authGuard, auditLogGuard, auditLogController.store);
 
 export default router;
--- src/audit-log/audit-log.service.ts
+++ src/audit-log/audit-log.service.ts
@@ -1,1 +1,15 @@
 import { connection } from '../app/database/mysql';
+import { AuditLogModel } from './audit-log.model';
+
+/**
+ * 创建审核日志
+ */
+export const createAuditLog = async (auditLog: AuditLogModel) => {
+  const statement = `
+    INSERT INTO audit_log
+    SET ?
+  `;
+
+  const [data] = await connection.promise().query(statement, auditLog);
+  return data;
+};
