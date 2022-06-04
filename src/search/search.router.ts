419c792f6f8ee914e70b0cb1a8d5486f1d35e4ca
于曼丽 <yumanli@126.com>
2022-06-04 17:48:28 +0800

    记录搜索相关接口的使用情况

--- src/search/search.router.ts
+++ src/search/search.router.ts
@@ -1,11 +1,44 @@
 import { Router } from 'express';
+import { accessLog } from '../access-log/access-log.middleware';
 import * as searchController from './search.controller';
 
 const router = Router();
 
-router.get('/search/tags', searchController.tags);
-router.get('/search/users', searchController.users);
-router.get('/search/cameras', searchController.cameras);
-router.get('/search/lens', searchController.lens);
+router.get(
+  '/search/tags',
+  accessLog({
+    action: 'searchTags',
+    resourceType: 'search',
+    payloadParam: 'query.name',
+  }),
+  searchController.tags,
+);
+router.get(
+  '/search/users',
+  accessLog({
+    action: 'searchUsers',
+    resourceType: 'search',
+    payloadParam: 'query.name',
+  }),
+  searchController.users,
+);
+router.get(
+  '/search/cameras',
+  accessLog({
+    action: 'searchCameras',
+    resourceType: 'search',
+    payloadParam: 'query.model',
+  }),
+  searchController.cameras,
+);
+router.get(
+  '/search/lens',
+  accessLog({
+    action: 'searchLens',
+    resourceType: 'search',
+    payloadParam: 'query.model',
+  }),
+  searchController.lens,
+);
 
 export default router;
