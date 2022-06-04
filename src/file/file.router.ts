5ffc9689faef1ad7c9fd5d827956161170455eb3
于曼丽 <yumanli@126.com>
2022-06-04 17:24:08 +0800

    记录上传文件与获取文件信息接口的使用情况

--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -2,6 +2,7 @@ import express = require('express');
 import * as fileController from './file.controller';
 import { authGuard } from '../auth/auth.middleware';
 import { fileInterceptor, fileProcessor } from './file.middleware';
+import { accessLog } from '../access-log/access-log.middleware';
 
 const router = express.Router();
 
@@ -10,9 +11,21 @@ router.post(
   authGuard,
   fileInterceptor,
   fileProcessor,
+  accessLog({
+    action: 'createFile',
+    resourceType: 'file',
+  }),
   fileController.store,
 );
 router.get('/files/:fileId/serve', fileController.serve);
-router.get('/files/:fileId/metadata', fileController.metadata);
+router.get(
+  '/files/:fileId/metadata',
+  accessLog({
+    action: 'getFileMetadata',
+    resourceType: 'file',
+    resourceParamName: 'fileId',
+  }),
+  fileController.metadata,
+);
 
 export default router;
