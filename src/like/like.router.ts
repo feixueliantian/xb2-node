c560d8f8c1c81e956b1ecf6f9c1769b3d90bea78
于曼丽 <yumanli@126.com>
2022-06-04 17:39:14 +0800

    记录内容点赞接口的使用情况

--- src/like/like.router.ts
+++ src/like/like.router.ts
@@ -1,13 +1,28 @@
 import { Router } from 'express';
+import { accessLog } from '../access-log/access-log.middleware';
 import { authGuard } from '../auth/auth.middleware';
 import * as likeController from './like.controller';
 
 const router = Router();
 
-router.post('/posts/:postId/like', authGuard, likeController.storeUserLikePost);
+router.post(
+  '/posts/:postId/like',
+  authGuard,
+  accessLog({
+    action: 'createUserLikePost',
+    resourceType: 'post',
+    resourceParamName: 'postId',
+  }),
+  likeController.storeUserLikePost,
+);
 router.delete(
   '/posts/:postId/like',
   authGuard,
+  accessLog({
+    action: 'deleteUserLikePost',
+    resourceType: 'post',
+    resourceParamName: 'postId',
+  }),
   likeController.destroyUserLikePost,
 );
 export default router;
