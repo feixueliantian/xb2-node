a55da8929f16a3e2a5484b61cd083e3ffcab69e6
于曼丽 <yumanli@126.com>
2022-06-04 17:49:42 +0800

    记录标签相关接口的使用情况

--- src/tag/tag.router.ts
+++ src/tag/tag.router.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 import * as tagController from './tag.controller';
 import { authGuard } from '../auth/auth.middleware';
+import { accessLog } from '../access-log/access-log.middleware';
 
 const router = Router();
 
-router.post('/tags', authGuard, tagController.store);
+router.post(
+  '/tags',
+  authGuard,
+  accessLog({
+    action: 'createTag',
+    resourceType: 'tag',
+    payloadParam: 'body.name',
+  }),
+  tagController.store,
+);
 
 export default router;
