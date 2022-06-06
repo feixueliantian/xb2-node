f8f66b80e4434762cd8ca1c89c5547e93b59f10d
于曼丽 <yumanli@126.com>
2022-06-06 11:33:43 +0800

    定义访问次数列表方法（ getAccessCounts）

--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,1 +1,51 @@
+import { access } from 'fs';
 import { connection } from '../app/database/mysql';
+import { AccessCountListItem, allowedAccessCounts } from './dashboard.provider';
+
+/**
+ * 获取访问次数列表
+ */
+interface GetAccessCountsOptions {
+  filter: {
+    name: string;
+    sql?: string;
+    param?: string;
+  };
+}
+
+export const getAccessCounts = async (options: GetAccessCountsOptions) => {
+  const {
+    filter: { sql: whereDatetimeRange },
+  } = options;
+
+  // 允许的动作
+  const allowedActions = allowedAccessCounts
+    .map((allowedAccess) => `'${allowedAccess.action}'`)
+    .join(',');
+
+  // 允许的动作条件
+  const andWhereActionIn = `AND where action in ${allowedActions}`;
+
+  const statement = `
+    SELECT
+      access_log.action,
+      COUNT(access_log.id) AS value
+    FROM
+      access_log
+    WHERE
+      ${whereDatetimeRange} ${andWhereActionIn}
+    GROUP BY
+      access_log.action
+  `;
+
+  const [data] = await connection.promise().query(statement);
+  const results = data as Array<AccessCountListItem>;
+
+  return allowedAccessCounts.map((allowedAccess) => {
+    const result = results.find(
+      (result) => result.action === allowedAccess.action,
+    );
+    allowedAccess.value = result && result.value ? result.value : 0;
+    return allowedAccess;
+  });
+};
