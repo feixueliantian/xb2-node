6fae8ed2a59d3f8720d804b1d7f7d78842dd66c8
于曼丽 <yumanli@126.com>
2022-06-07 11:15:48 +0800

    定义按 Info 字段值查询用户 Meta 的方法（getUserMetaByInfoProp，getUserMetaByWeixinUnionId）

--- src/user-meta/user-meta-service.ts
+++ src/user-meta/user-meta-service.ts
@@ -1,5 +1,5 @@
 import { connection } from '../app/database/mysql';
-import { UserMetaModel } from './user-meta.model';
+import { UserMetaModel, UserMetaType } from './user-meta.model';
 
 /**
  * 创建用户 Meta
@@ -33,3 +33,42 @@ export const updateUserMeta = async (
 
   return data as any;
 };
+
+/**
+ * 按 Info 字段的某个 Prop 查询 UserMeta
+ */
+export const getUserMetaByInfoProp = (
+  type: UserMetaType,
+  infoPropName: string,
+) => {
+  return async (value: number | string) => {
+    const statement = `
+      SELECT
+        *
+      FROM
+        user_meta
+      WHERE
+        type = ?
+        AND user_meta.info->'$.${infoPropName}' = ?
+      ORDER BY
+        user_meta.id DESC
+      LIMIT 1
+    `;
+
+    const params = [type, value];
+    const [data] = await connection.promise().query(statement, params);
+    return data[0] ? (data[0] as any) : null;
+  };
+};
+
+export const getUserMetaByWeixinOpenId = getUserMetaByInfoProp(
+  UserMetaType.weixinUserInfo,
+  // openid 加密之后的微信用户的账户
+  'openid',
+);
+
+export const getUserMetaByWeixinUnionId = getUserMetaByInfoProp(
+  UserMetaType.weixinUserInfo,
+  // unionid 微信用户在所有应用里面的统一标识
+  'unionid',
+);
