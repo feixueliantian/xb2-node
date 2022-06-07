e05f5fd54c7175e2f8cb0c1efc27a707c6c17659
于曼丽 <yumanli@126.com>
2022-06-07 11:46:17 +0800

    测试用户 Meta 相关方法

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,4 +1,10 @@
 import { Router } from 'express';
+import {
+  createUserMeta,
+  getUserMetaByWeixinUnionId,
+  updateUserMeta,
+} from '../user-meta/user-meta-service';
+import { UserMetaModel, UserMetaType } from '../user-meta/user-meta.model';
 
 const router = Router();
 
@@ -6,8 +12,10 @@ router.get('/', (request, response) => {
   response.send({ title: '小白兔的开发之路' });
 });
 
-router.post('/echo', (request, response) => {
-  response.status(201).send(request.body);
+router.post('/echo', async (request, response) => {
+  const userMeta = await getUserMetaByWeixinUnionId('321');
+
+  response.send(userMeta);
 });
 
 export default router;
