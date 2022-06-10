64ec20f0c1526351ee5d920a7f2066f06d9a8154
于曼丽 <yumanli@126.com>
2022-06-10 15:59:23 +0800

    定义按 ID 调取订单的方法（getOrderById）

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -13,3 +13,20 @@ export const createOrder = async (order: OrderModel) => {
   const [data] = await connection.promise().query(statement, order);
   return data as any;
 };
+
+/**
+ * 按照 ID 获取订单
+ */
+export const getOrderById = async (orderId: number) => {
+  const statement = `
+    SELECT
+      *
+    FROM
+      \`order\`
+    WHERE
+      order.id = ?
+  `;
+
+  const [data] = await connection.promise().query(statement, orderId);
+  return data[0] as OrderModel;
+};
