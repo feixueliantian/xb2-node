55b7f9708550962a90a4acebb36c2872cb3a809e
于曼丽 <yumanli@126.com>
2022-06-10 17:40:12 +0800

    创建与更新订单时记录订单日志

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import { OrderLogAction } from '../order-log/order-log.model';
+import { createOrderLog } from '../order-log/order-log.service';
 import { createOrder, updateOrder } from './order.service';
 
 /**
@@ -9,10 +11,25 @@ export const store = async (
   response: Response,
   next: NextFunction,
 ) => {
-  const { order } = request.body;
+  const { order, resourceType, resourceId } = request.body;
+  const { user } = request;
 
   try {
+    // 创建订单
     const data = await createOrder(order);
+
+    // 创建订单日志
+    await createOrderLog({
+      userId: user.id,
+      orderId: data.insertId,
+      action: OrderLogAction.orderCreated,
+      meta: JSON.stringify({
+        ...order,
+        resourceType,
+        resourceId,
+      }),
+    });
+
     return response.status(201).send(data);
   } catch (error) {
     return next(error);
@@ -28,9 +45,22 @@ export const update = async (
   next: NextFunction,
 ) => {
   const { dataForUpdate, order } = request.body;
+  const { user } = request;
 
   try {
+    // 更新订单
     const data = await updateOrder(order.id, dataForUpdate);
+
+    // 创建订单日志
+    await createOrderLog({
+      userId: user.id,
+      orderId: order.id,
+      action: OrderLogAction.orderUpdated,
+      meta: JSON.stringify({
+        ...dataForUpdate,
+      }),
+    });
+
     return response.send(data);
   } catch (error) {
     return next(error);
