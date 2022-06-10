1e94bb2220e6083a2c3736bd3ea4946e34668192
于曼丽 <yumanli@126.com>
2022-06-10 15:15:14 +0800

    定义创建订单方法（createOrder）

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,1 +1,15 @@
 import { connection } from '../app/database/mysql';
+import { OrderModel } from './order.model';
+
+/**
+ * 创建订单
+ */
+export const createOrder = async (order: OrderModel) => {
+  const statement = `
+    INSERT INTO \`order\`
+    SET ?
+  `;
+
+  const [data] = await connection.promise().query(statement, order);
+  return data as any;
+};
