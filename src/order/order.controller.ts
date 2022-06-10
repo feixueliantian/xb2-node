f276996fe36092448ea5daf3755659d23ed042f1
于曼丽 <yumanli@126.com>
2022-06-10 15:54:40 +0800

    定义创建订单接口

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,1 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
+import { createOrder } from './order.service';
+
+/**
+ * 创建订单
+ */
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  const { order } = request.body;
+
+  try {
+    const data = await createOrder(order);
+    return response.status(201).send(data);
+  } catch (error) {
+    return next(error);
+  }
+};
--- src/order/order.router.ts
+++ src/order/order.router.ts
@@ -1,6 +1,21 @@
 import { Router } from 'express';
+import { accessLog } from '../access-log/access-log.middleware';
+import { authGuard } from '../auth/auth.middleware';
 import * as orderController from './order.controller';
+import { orderGuard } from './order.middleware';
 
 const router = Router();
+
+// 创建订单
+router.post(
+  '/orders',
+  authGuard,
+  orderGuard,
+  accessLog({
+    action: 'createOrder',
+    resourceType: 'order',
+  }),
+  orderController.store,
+);
 
 export default router;
