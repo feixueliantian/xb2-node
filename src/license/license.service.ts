452427f4377c0cc0129c355ce4e505d900e8ff47
于曼丽 <yumanli@126.com>
2022-06-11 11:26:13 +0800

    定义按订单 ID 调取许可方法（getLicenseByOrderId）

--- src/license/license.service.ts
+++ src/license/license.service.ts
@@ -33,3 +33,20 @@ export const updateLicense = async (
 
   return data as any;
 };
+
+/**
+ * 按照订单 ID 获取许可
+ */
+export const getLicenseByOrderId = async (orderId: number) => {
+  const statement = `
+    SELECT
+      *
+    FROM
+      license
+    WHERE
+      license.orderId = ?
+  `;
+
+  const [data] = await connection.promise().query(statement, orderId);
+  return data[0] as LicenseModel;
+};
