361f7e382572e6915974f5e2a5c41b93935c894c
于曼丽 <yumanli@126.com>
2022-06-11 11:21:49 +0800

    定义更新许可方法（updateLicense）

--- src/license/license.service.ts
+++ src/license/license.service.ts
@@ -13,3 +13,23 @@ export const createLicense = async (license: LicenseModel) => {
   const [data] = await connection.promise().query(statement, license);
   return data as any;
 };
+
+/**
+ * 更新许可
+ */
+export const updateLicense = async (
+  licenseId: number,
+  license: LicenseModel,
+) => {
+  const statement = `
+    UPDATE license
+    SET ?
+    WHERE license.id = ?
+  `;
+
+  const [data] = await connection
+    .promise()
+    .query(statement, [license, licenseId]);
+
+  return data as any;
+};
