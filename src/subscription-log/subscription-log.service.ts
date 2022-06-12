81bec5a2fbe1f12e30afe789e3dbc4376657cecd
于曼丽 <yumanli@126.com>
2022-06-12 17:45:03 +0800

    定义按订单 ID 调取订阅日志方法（getSubscriptionLogByOrderId）

--- src/subscription-log/subscription-log.service.ts
+++ src/subscription-log/subscription-log.service.ts
@@ -15,3 +15,23 @@ export const createSubscriptionLog = async (
   const [data] = await connection.promise().query(statement, subscriptionLog);
   return data as any;
 };
+
+/**
+ * 按照订单 ID 调取订阅日志
+ */
+export const getSubscriptionLogByOrderId = async (orderId: number) => {
+  const statement = `
+    SELECT
+      *
+    FROM
+      subscription_log
+    WHERE
+      subscription_log.orderId = ?
+    ORDER BY
+      subscription_log.id DESC
+    LIMIT 1 
+  `;
+
+  const [data] = await connection.promise().query(statement, orderId);
+  return data[0] as SubscriptionLogModel;
+};
