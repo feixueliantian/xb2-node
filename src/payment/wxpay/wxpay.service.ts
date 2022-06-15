c4902cb9d3b5acf37de22b959c2390313b2313aa
于曼丽 <yumanli@126.com>
2022-06-15 09:47:32 +0800

    定义微信支付方法（wxpay）

--- src/payment/wxpay/wxpay.service.ts
+++ src/payment/wxpay/wxpay.service.ts
@@ -1,7 +1,21 @@
 import querystring = require('querystring');
 import crypto = require('crypto');
-import { WxpayOrder, WxpayPaymentResult } from './wxpay.interface';
-import { WXPAY_KEY } from '../../app/app.config';
+import { Request } from 'express';
+import {
+  WxpayOrder,
+  WxpayPaymentResult,
+  WxpayPrePayResult,
+} from './wxpay.interface';
+import {
+  APP_NAME,
+  WXPAY_API_UNIFIEDORDER,
+  WXPAY_APP_ID,
+  WXPAY_KEY,
+  WXPAY_MCH_ID,
+  WXPAY_NOTIFY_URL,
+} from '../../app/app.config';
+import { OrderModel } from '../../order/order.model';
+import { httpClient, uid, xmlBuilder, xmlParser } from '../../app/app.service';
 
 /**
  * 微信支付：签名
@@ -49,4 +63,76 @@ export const wxpayVerifyPaymentResult = async (
   const isValidSign = sign === selfSign;
 
   return isValidSign;
+};
+
+/**
+ * 微信支付
+ */
+export const wxpay = async (order: OrderModel, request: Request) => {
+  // 公众账号 ID
+  const appid = WXPAY_APP_ID;
+
+  // 微信支付商户 ID
+  const mch_id = WXPAY_MCH_ID;
+
+  // 订单号
+  const out_trade_no = `${uid()}_${order.id}`;
+
+  // 商品 ID
+  const product_id = order.productId;
+
+  // 商品描述
+  const body = `${APP_NAME}#${order.id}`;
+
+  // 订单金额
+  const total_fee = Math.round(order.totalAmount * 100);
+
+  // 支付类型（扫码支付）
+  const trade_type = 'NATIVE';
+
+  // 通知地址
+  const notify_url = WXPAY_NOTIFY_URL;
+
+  // 随机字符
+  const nonce_str = uid();
+
+  // 附加数据
+  const socketId = (request.headers['x-socket-id'] ||
+    request.headers['X-Socket-Id']) as string;
+
+  const attach = socketId || 'NULL';
+
+  // 订单
+  const wxpayOrder = {
+    appid,
+    mch_id,
+    out_trade_no,
+    product_id,
+    body,
+    total_fee,
+    trade_type,
+    notify_url,
+    nonce_str,
+    attach,
+  };
+
+  // 签名
+  const sign = wxpaySign(wxpayOrder, WXPAY_KEY);
+
+  const wxpayOrderXml = xmlBuilder.buildObject({
+    ...wxpayOrder,
+    sign,
+  });
+
+  // 统一下单
+  const response = await httpClient.post(WXPAY_API_UNIFIEDORDER, wxpayOrderXml);
+  const { xml: prepayResult } = await xmlParser.parseStringPromise(
+    response.data,
+  );
+
+  if (prepayResult.return_code === 'FAIL') {
+    throw new Error(prepayResult.return_msg);
+  }
+
+  return prepayResult as WxpayPrePayResult;
 };
