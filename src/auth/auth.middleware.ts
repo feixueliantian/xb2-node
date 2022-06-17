156a6509f05f2c67b9b350e81860171d667f628f
于曼丽 <yumanli@126.com>
2022-06-17 10:31:43 +0800

    改进：让权限控制中间件支付检查是否为管理员

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -82,18 +82,23 @@ export const currentUser = (
 
 interface AccessControlOptions {
   possession?: boolean;
+  isAdmin?: boolean;
 }
 
 export const accessControl = (options: AccessControlOptions) => {
   return async (request: Request, response: Response, next: NextFunction) => {
     // 解构选项
-    const { possession } = options;
+    const { possession, isAdmin } = options;
 
     // 当前用户 ID
     const { id: userId } = request.user;
 
     // 放行管理员
     if (userId == 1) return next();
+
+    if (isAdmin) {
+      if (userId !== 1) return next(new Error('FORBIDDEN'));
+    }
 
     // 准备资源
     const resourceIdParam = Object.keys(request.params)[0];
